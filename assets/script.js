775f2ba0dc434735faa93fbfa6bb47a5693ea65a
santhosh <144583697+santhoshofcl@users.noreply.github.com>
2024-07-30 12:03:58 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+function updateNavLinks() {
+    let scrollPosition = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150; 
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (scrollPosition >= offset && scrollPosition < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href').includes(id)) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+}
+
+window.addEventListener('scroll', updateNavLinks);
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
